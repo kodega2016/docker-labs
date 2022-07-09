f12aca33ffdbba41953d9ba243c0305cb560fa9c
kodega2016 <khadgalovecoding2016@gmail.com>
2022-07-09 08:18:21 +0545

    Simple dockerize node app

---
+++ simpleweb/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        success: true,
+        data: [],
+        message: 'Server is running successfully...'
+    })
+})
+
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000')
+});
